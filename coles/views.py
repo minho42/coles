fe9b4c19a48774ecc0679aba403b66888d088797
Min ho Kim <minho42@gmail.com>
2022-06-04 21:47:36 +0900

    fix: add () to call which_settings()

--- coles/views.py
+++ coles/views.py
@@ -45,7 +45,7 @@ class ColesListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["is_production"] = which_settings == "production"
+        context["is_production"] = which_settings() == "production"
         return context
 
 
