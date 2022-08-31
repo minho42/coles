f17965817c179776622493b8b7d341d4c9781ed6
Min ho Kim <minho42@gmail.com>
2022-08-31 11:00:05 +1000

    remove heroku related stuff

--- settings/production.py
+++ settings/production.py
@@ -1,15 +1,14 @@
 from .base import *
 
-import django_heroku
 
 DEBUG = False
 
 ALLOWED_HOSTS += [
-    "coles2.herokuapp.com",
+    "",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://coles2.herokuapp.com",
+    "",
 ]
 
 # Security
@@ -18,7 +17,7 @@ SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
-# SECURE_SSL_REDIRECT = True // <- commented out for heroku deployment as it causes "ERR_TOO_MANY_REDIRECTS"
+SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_HSTS_PRELOAD = True
@@ -27,5 +26,3 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_SECONDS = 3600
 SECURE_REFERRER_POLICY = "same-origin"
 
-
-django_heroku.settings(locals())
